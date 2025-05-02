0e4e57c7243abc732a97b0606c118a4d32e58094
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-02 15:23:57 -0300

    Exception handling - delete

--- src/main/java/com/projetospring/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetospring/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.projetospring.course.resources.exceptions;
 
+import com.projetospring.course.services.exceptions.DataBaseException;
 import com.projetospring.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.*;
@@ -15,6 +16,16 @@ public class ResourceExceptionHandler {
     resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
     String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(),
+                error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError>
+    dataBase(DataBaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(),
                 error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
---
+++ src/main/java/com/projetospring/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.projetospring.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException(String msg) {
+        super(msg);
+    }
+}
